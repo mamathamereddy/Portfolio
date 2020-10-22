ad766ff83340cb193e4c41b63fe5ab3e392ae9f6
mamathamereddy <mamathavangala1@gmail.com>
2020-10-22 13:00:26 +0200

    adding footer

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import About from "./components/About";
 import Education from "./components/Education";
 import Projects from "./components/Projects";
 import Experience from "./components/Experience";
+import Footer from './components/Footer'
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
       <Projects />
       <Experience />
       <Education />
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div class="footer">
+        <p class="footer__text">Made by Mamatha Mereddy @2020 
+        <a href="https://github.com/Laila1222" class="footer__link" target="_blank" rel="noopener noreferrer">
+            </a>
+            </p>
+            </div>
+    )
+}
+
+export default Footer
+
