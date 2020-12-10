459f90264a7a3a6be263dd3fd980000d05fd3ed6
mamathamereddy <mamathavangala1@gmail.com>
2020-12-10 11:06:27 +0100

    refactoring the gitdeploy url

--- src/data/Projects.js
+++ src/data/Projects.js
@@ -1,67 +1,61 @@
-import hotel from '../images/hotel.png';
-import weather from '../images/weather.png';
-import meal from '../images/mealsharing.jpg'
-import fastestpresser from '../images/fastestpresser.jpg'
-import project from '../images/project.jpg'
-import github from '../images/github.jpg'
-
-
+import hotel from "../images/hotel.png";
+import weather from "../images/weather.png";
+import meal from "../images/mealsharing.jpg";
+import fastestpresser from "../images/fastestpresser.jpg";
+import project from "../images/project.jpg";
+import github from "../images/github.jpg";
 
 const projects = [
-	{
-		id: 1,
-		name:"Hotel Booking",
-		image:hotel,
-	    type:"MERN Stack",
-        githubUrl:'https://github.com/mamathamereddy/HotelBooking-Client',
-        deployedUrl:"hotel-bookingapp.netlify.app/"
-	},
-	
-	{
-		id: 2,
-		name:"HackYourFutureProject",
-		image: project,
-	    type:"FullStack",
-        githubUrl:'https://github.com/mamathamereddy/FP-class11',
-        deployedUrl:"https://fp-class11.herokuapp.com/"
-		
-	},
-	{
-		id: 3,
-		name:"github user searcher",
-		image:github ,
-	    type:"Reactjs",
-        githubUrl:'https://github.com/mamathamereddy/github-user-searcher',
-        deployedUrl:"https://react-github-user-search-app.herokuapp.com/"
-	},
-	{
-		id: 4,
-		name:"Meal sharing",
-		image: meal,
-	    type:"Node/Express/Mysql",
-        githubUrl:'https://github.com/mamathamereddy/mealsharing',
-		deployedUrl:"https://hyf-meals-sharing.herokuapp.com/"
-	},
-	{
-		id: 5,
-		name:"Weather App",
-		image:weather,
-	    type:"JavaScript",
-        githubUrl:'https://github.com/mamathamereddy/weather-app',
-        deployedUrl:"https://mamathamereddy.github.io/weather-app/"
-	},
-	
-	{
-		id: 6,
-		name:"Fastest Presser",
-		image: fastestpresser,
-	    type:"javaScript",
-        githubUrl:'https://github.com/mamathamereddy/fastestPresser',
-        deployedUrl:"https://mamathamereddy.github.io/fastestPresser/"
-	},
-	
-	
+  {
+    id: 1,
+    name: "Hotel Booking",
+    image: hotel,
+    type: "MERN Stack",
+    githubUrl: "https://github.com/mamathamereddy/HotelBooking-Client",
+    deployedUrl: "https://hotel-bookingapp.netlify.app/",
+  },
+
+  {
+    id: 2,
+    name: "HackYourFutureProject",
+    image: project,
+    type: "FullStack",
+    githubUrl: "https://github.com/mamathamereddy/FP-class11",
+    deployedUrl: "https://fp-class11.herokuapp.com/",
+  },
+  {
+    id: 3,
+    name: "github user searcher",
+    image: github,
+    type: "Reactjs",
+    githubUrl: "https://github.com/mamathamereddy/github-user-searcher",
+    deployedUrl: "https://react-github-user-search-app.herokuapp.com/",
+  },
+  {
+    id: 4,
+    name: "Meal sharing",
+    image: meal,
+    type: "Node/Express/Mysql",
+    githubUrl: "https://github.com/mamathamereddy/mealsharing",
+    deployedUrl: "https://hyf-meals-sharing.herokuapp.com/",
+  },
+  {
+    id: 5,
+    name: "Weather App",
+    image: weather,
+    type: "JavaScript",
+    githubUrl: "https://github.com/mamathamereddy/weather-app",
+    deployedUrl: "https://mamathamereddy.github.io/weather-app/",
+  },
+
+  {
+    id: 6,
+    name: "Fastest Presser",
+    image: fastestpresser,
+    type: "javaScript",
+    githubUrl: "https://github.com/mamathamereddy/fastestPresser",
+    deployedUrl: "https://mamathamereddy.github.io/fastestPresser/",
+  },
 ];
 
-export default projects
-
+export default projects;
