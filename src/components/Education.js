c851277d29b488bd04636706fc9e57382de5f323
mamathamereddy <mamathavangala1@gmail.com>
2021-01-13 10:00:00 +0100

    updating education

--- src/components/Education.js
+++ src/components/Education.js
@@ -31,11 +31,25 @@ const Education = () => {
             </div>
 
             <input type="checkbox" id="title2" checked />
-            <label for="title2">Masters in computer Applications</label>
+            <label for="title2">
+              Masters in computer Applications Osmania University,India
+              <span> 2004-2007</span>
+            </label>
 
             <div class="content">
               <p>
                 Osmania University,India<span> 2004-2007</span>
+              </p>
+              <p>
+                <b>Project:"TechnoTask Management"</b>
+                <p>
+                  <b>Stack:ASP.NET with C#,SQL Server 2000 with IIS Server</b>
+                </p>
+                Techno Task Management is a webbased applicaton where in
+                employees spans over in different departments.This application
+                is desined for such an environment where the work is divided
+                into group of employees and during the course of division the
+                employees are selected to be part of the work in hand.
               </p>
             </div>
 
