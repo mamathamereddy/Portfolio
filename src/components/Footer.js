fc17ada2d20cbc985e2f18f600e54b679bb976a8
mamathamereddy <mamathavangala1@gmail.com>
2021-01-14 10:00:00 +0100

    updating footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
-    return (
-        <div class="footer">
-        <p class="footer__text">Made by Mamatha Mereddy @2020 
-            </p>
-            </div>
-    )
-}
+  return (
+    <div class="footer">
+      <p class="footer__text">Made by Mamatha Mereddy</p>
+    </div>
+  );
+};
 
-export default Footer
-
+export default Footer;
