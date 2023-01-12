cf6f6746ccb1f83bdf2489d268e49d5f56922d5f
mamathamereddy <mamathavangala1@gmail.com>
2023-01-12 14:43:00 +0100

    hiding the links that are nor working

--- src/data/Projects.js
+++ src/data/Projects.js
@@ -8,15 +8,6 @@ import github from "../images/github.jpg";
 const projects = [
   {
     id: 1,
-    name: "Hotel Booking",
-    image: hotel,
-    type: "MERN Stack",
-    githubUrl: "https://github.com/mamathamereddy/HotelBooking-Client",
-    deployedUrl: "https://hotel-bookingapp.netlify.app/",
-  },
-
-  {
-    id: 2,
     name: "Fastest Presser",
     image: fastestpresser,
     type: "JavaScript",
@@ -24,38 +15,46 @@ const projects = [
     deployedUrl: "https://mamathamereddy.github.io/fastestPresser/",
   },
   {
-    id: 3,
+    id: 2,
     name: "github user searcher",
     image: github,
     type: "Reactjs",
     githubUrl: "https://github.com/mamathamereddy/github-user-searcher",
-    deployedUrl: "https://react-github-user-search-app.herokuapp.com/",
+    deployedUrl: "https://user-searcher-github.netlify.app",
   },
   {
-    id: 4,
-    name: "Meal sharing",
-    image: meal,
-    type: "Node/Express/Mysql",
-    githubUrl: "https://github.com/mamathamereddy/mealsharing",
-    deployedUrl: "https://hyf-meals-sharing.herokuapp.com/",
-  },
-  {
-    id: 5,
+    id: 3,
     name: "Weather App",
     image: weather,
     type: "JavaScript",
     githubUrl: "https://github.com/mamathamereddy/weather-app",
     deployedUrl: "https://mamathamereddy.github.io/weather-app/",
   },
+  // {
+  //   id: 4,
+  //   name: "Hotel Booking",
+  //   image: hotel,
+  //   type: "MERN Stack",
+  //   githubUrl: "https://github.com/mamathamereddy/HotelBooking-Client",
+  //   deployedUrl: "https://hotel-bookingapp.netlify.app/",
+  // },
+  // {
+  //   id: 5,
+  //   name: "Meal sharing",
+  //   image: meal,
+  //   type: "Node/Express/Mysql",
+  //   githubUrl: "https://github.com/mamathamereddy/mealsharing",
+  //   deployedUrl: "https://hyf-meals-sharing.herokuapp.com/",
+  // },
 
-  {
-    id: 6,
-    name: "HackYourFutureProject",
-    image: project,
-    type: "FullStack",
-    githubUrl: "https://github.com/mamathamereddy/FP-class11",
-    deployedUrl: "https://fp-class11.herokuapp.com/",
-  },
+  // {
+  //   id: 6,
+  //   name: "HackYourFutureProject",
+  //   image: project,
+  //   type: "FullStack",
+  //   githubUrl: "https://github.com/mamathamereddy/FP-class11",
+  //   deployedUrl: "https://fp-class11.herokuapp.com/",
+  // },
 ];
 
 export default projects;
